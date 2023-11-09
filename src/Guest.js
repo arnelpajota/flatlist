5cf419b021c38ed46abe93f9bf3c146af896a386
arnelpajota <143138111+arnelpajota@users.noreply.github.com>
2023-11-09 20:38:39 +0800

    Add files via upload

---
+++ src/Guest.js
@@ -0,0 +1,76 @@
+import react from "react";
+
+export const Guest = [
+    {
+      name: {
+        fname: " NIKKO BRYLLE",
+        mname: "",
+        lname: "ALTURAS",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      name: {
+        fname: " CHARIZ MAE",
+        mname: "T",
+        lname: "BACO",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      name: {
+        fname: " ANALIE",
+        mname: "",
+        lname: "BARBANIDA",
+      },
+      address: "Las Vegas, Nevada",
+    },
+    {
+      name: {
+        fname: " RIZA",
+        mname: "",
+        lname: "BATULANON",
+      },
+      address: "Paris, France",
+    },
+    {
+      name: {
+        fname: " NIKKO BRYLLE",
+        mname: "",
+        lname: "ALTURAS",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      name: {
+        fname: " CHARIZ MAE",
+        mname: "T",
+        lname: "BACO",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      name: {
+        fname: " ANALIE",
+        mname: "",
+        lname: "BARBANIDA",
+      },
+      address: "Las Vegas, Nevada",
+    },
+    {
+      name: {
+        fname: " RIZA",
+        mname: "",
+        lname: "BATULANON",
+      },
+      address: "Paris, France",
+    },
+    {
+      name: {
+        fname: " NIKKO BRYLLE",
+        mname: "",
+        lname: "ALTURAS",
+      },
+      address: "Los Angeles, California",
+    },
+  ];
